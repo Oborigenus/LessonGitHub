814d55bbf51639179f76ab3d82d08e6d477fcdff
Oborigenus <s.shakeyev@mail.ru>
2025-03-14 21:06:32 +0500

    git.init

---
+++ AreaOfShapes/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void AreaFinder()
+{
+    while (true)
+    {
+        
+    }
+}
+Console.WriteLine("Enter your name");
+var name = Console.ReadLine();
+string[] namearray = ["Hello ", "!"];
+var nameanswer = string.Join( name , namearray);
+Console.WriteLine(nameanswer);
+Console.WriteLine("Now you must choose a shape for which we will find its area");
+tryAgain:
+try
+{
+    AreaFinder();
+}
+catch (Exception)
+{
+    Console.WriteLine("Wrong input, please try again");
+    goto tryAgain;
+}
+
+
